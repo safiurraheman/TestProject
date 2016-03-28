58c97ecdd0946caed0a99b8e4bc3781ba736f94d
skmohammed_raheman <skmohammed_raheman@apple.com>
2016-03-28 12:53:20 +0530

    first commit for testing

---
+++ TestProject/src/com/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestClass {
+
+	public void method1(){
+		System.out.println("In Method1");
+	}
+}
---
+++ TestProject/src/com/test/TestMainClass.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class TestMainClass {
+
+	public static void main(String[] args) {
+		TestClass tc1 = new TestClass();
+		tc1.method1();
+		method2();
+	}
+	
+	public static void method2(){
+		System.out.println("In method2");
+	}
+
+}
